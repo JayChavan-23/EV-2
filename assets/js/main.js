112fe93574717b40887953f6153d253da379690f
JayChavan-23 <jaychavan2223@gmail.com>
2022-06-26 23:52:22 +0530

    filters done

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,9 +60,22 @@ let swiperPopular = new Swiper(".popular__container", {
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
-
+let mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 /* Link active featured */ 
+const linkFeatured = document.querySelectorAll('.featured__item')
 
+function activeFeatured(){
+    linkFeatured.forEach(l=> l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
